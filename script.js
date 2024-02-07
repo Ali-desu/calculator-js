015fef9f6cedf476cf9537bf598b2e1504268f89
Ali <aliadnani056@gmail.com>
2024-02-07 01:24:11 +0100

    calculator-incomplete

---
+++ script.js
@@ -0,0 +1,51 @@
+//calculator
+const buttons = document.getElementsByTagName('button');
+const screen = document.querySelector('#screen');
+let result = 0;
+let i = 0;
+
+
+
+
+
+function appendValue(value,e) {
+   
+    if(i>0 && e.target.classList.contains('operation') && screen.value[i-1] == value){
+        if( value != "-"){
+            screen.value = screen.value.toString().slice(0,i);
+        }
+        else{
+            screen.value = screen.value.toString().slice(0,i-1) + '+';
+        }
+        
+    }
+    
+    else{
+    screen.value += value;
+    i++;
+    }
+}
+
+
+for (const button of buttons) {
+    button.addEventListener('click', function (e) {
+        if (e.target.id === 'AC') {
+            screen.value = '';
+            i=0;
+        }
+
+        else if (e.target.id === 'DEL') {
+            screen.value = screen.value.toString().slice(0,-1);
+            i--;
+        }
+
+        else if(e.target.classList.contains('calculate')){
+            screen.value = eval(screen.value);
+            i = screen.value.length;
+        }
+
+        else{
+           appendValue(e.target.textContent,e);
+        }
+    });
+}
